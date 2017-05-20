10b9f6454b3fe3ab5d4c42e83063194a1954fb6f
cpsdqs <cpsdqs@gmail.com>
2017-05-20 16:55:10 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# dash-ui theme
+
+A matching UI theme for [dash-syntax](https://atom.io/themes/dash-syntax).
+
+![Screenshot](https://i.imgur.com/crhLq9Z.png)
+![Settings Screenshot](https://i.imgur.com/NlHG13r.png)
---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+const gulp = require('gulp')
+const webfont = require('gulp-webfont')
+const cson = require('cson')
+const fs = require('fs')
+const path = require('path')
+
+// default color
+const COLOR = 'inherit';
+
+gulp.task('generate-less', function () {
+  let files = fs.readdirSync('icons/')
+  let colors = cson.parseCSONFile('icons/colors.cson')
+  let content = fs.readFileSync('styles/file-icons-header.less', 'utf8')
+  content += '.tree-view .file {\n' + files
+    .map(file => path.parse(file))
+    .filter(file => file.ext === '.svg')
+    .sort((a, b) => a.name.length - b.name.length)
+    .map(file => {
+      let type = 'file-icon'
+      let color = colors[file.name] || COLOR
+      if (Array.isArray(color)) {
+        type = `file-icon-${color[0]}-gradient`
+        color = `${color[0]}-gradient(${color.slice(1).join(', ')})`
+      }
+      return `  .${type}('${file.name}', ${color});`
+    })
+    .join('\n') + '\n}\n'
+  fs.writeFileSync('styles/file-icons.less', content)
+})
+
+gulp.task('icons', function () {
+  return gulp.src('icons/*.svg')
+    .pipe(webfont({
+      types: 'woff2',
+      ligatures: true
+    }))
+    .pipe(gulp.dest('fonts'))
+})
+
+gulp.task('default', ['icons', 'generate-less'])
